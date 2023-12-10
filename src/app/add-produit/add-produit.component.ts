2278f2e2e1aa3994efe1e009c41c04c0bfc3c297
Nordine Khoudou <nordine-khoudou@hotmail.fr>
2023-12-10 18:20:01 +0100

    Premier commit

---
+++ src/app/add-produit/add-produit.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
+import { Categorie } from '../model/categorie.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add-produit',
+  templateUrl: './add-produit.component.html'
+})
+export class AddProduitComponent implements OnInit {
+
+  newProduit = new Produit();
+  message?: string;
+  categories! : Categorie[];
+  newIdCat! : number;
+  newCategorie! : Categorie;
+
+  constructor(private produitService: ProduitService,
+    private router: Router){
+   
+  }
+
+  ngOnInit(): void {
+    // this.categories = this.produitService.listeCategories();
+  }
+
+  addProduit(){
+    this.produitService.ajouterProduit(this.newProduit)
+    .subscribe(prod => {
+    console.log(prod);
+    this.router.navigate(['produits']);
+    });
+    }
+    
+ 
+}
---
+++ src/app/services/produit.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Produit } from '../model/produit.model';
+import { Categorie } from '../model/categorie.model';
+
+const httpOptions = {
+  headers: new HttpHeaders( {'Content-Type': 'application/json'} )
+  };
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProduitService {
+
+  produits!: Produit[];
+  // categories : Categorie[];
+
+  apiURL: string = 'http://localhost:8080/produits/api';
+
+
+
+  constructor(private http : HttpClient){
+    // this.categories = [ 
+    //   {idCat : 1, nomCat : "PC"},
+    //   {idCat : 2, nomCat : "Imprimante"}
+    // ];
+
+    // this.produits = [
+    //   {idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011"), categorie : {idCat : 1, nomCat : "PC"}},
+    //   {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010"), categorie : {idCat : 2, nomCat : "Imprimante"}},
+    //   {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020"), categorie : {idCat : 1, nomCat : "PC"}}
+    // ]
+  }
+
+  // Observabke : Toutes les api les retournes (design pattern) facilite la gestion des appels async. Fournis une syntaxe concise.
+  listeProduit(): Observable<Produit[]>{
+    return this.http.get<Produit[]>(this.apiURL);
+  }
+
+  ajouterProduit( prod: Produit):Observable<Produit>{
+    return this.http.post<Produit>(this.apiURL, prod, httpOptions);
+  }
+
+  supprimerProduit( prod: Produit){
+    const index = this.produits.indexOf(prod, 0);
+    if (index > -1) {
+    this.produits.splice(index, 1);
+    }
+  }
+
+  consulterProduit(id:number): Produit{
+    return this.produits.find(p => p.idProduit == id)!;     
+    }
+
+  trierProduits(){
+    this.produits = this.produits.sort((n1,n2) => {
+    if (n1.idProduit! > n2.idProduit!) {
+    return 1;
+    }
+    if (n1.idProduit! < n2.idProduit!) {
+    return -1;
+    }
+    return 0;
+    });
+    }
+  
+  updateProduit(p:Produit)
+  {
+  this.supprimerProduit(p);
+  this.ajouterProduit(p);
+  this.trierProduits();
+  }
+
+  // listeCategories():Categorie[] {
+  //   return this.categories;
+  //   }
+  
+    
+  // consulterCategorie(id:number): Categorie{
+  //   return this.categories.find(cat => cat.idCat == id)!;
+  //   }
+}
